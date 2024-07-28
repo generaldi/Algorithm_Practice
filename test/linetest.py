77c7d04de1dd0d8e182acba5658b1611e46be64b
generaldi <general_di@163.com>
2024-07-28 12:06:50 +0800

    这是测试中文的例子

--- test/linetest.py
+++
@@ -1,8 +0,0 @@
-from pyecharts.charts import Line
-from pyecharts.faker import Faker
-line = Line()
-line.add_xaxis(Faker.choose())
-line.add_yaxis("商家A", Faker.values())
-line.add_yaxis("商家B", Faker.values())
-line.add_yaxis("商家C", Faker.values())
-line.render()
